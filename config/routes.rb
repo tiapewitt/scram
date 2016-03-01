2b35e8ace8806943bed24ba4e4629d2032fe1b82
Tia Pewitt <tiapewitt@gmail.com>
2016-03-01 10:09:20 -0500

    updated styling and stylesheets initializers

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'submissions#index'
+  root 'submissions#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'featured' => 'featured#index'
